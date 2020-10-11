7dcfc2dd348a8dabaf80b0da1acf538e480cc64f
sahossain99 <67564953+sahossain99@users.noreply.github.com>
2020-10-11 19:18:50 -0400

    Add files via upload

--- charts.js
+++ charts.js
@@ -66,15 +66,15 @@ function buildCharts(sample) {
     // Create a variable that filters the samples for the object with the desired sample number.
     var resultArray = samples.filter(sampleObj => sampleObj.id == sample);
     // 1. Create a variable that filters the metadata array for the object with the desired sample number.
-    var wfreqArray = data.metadata.map(d => d.wfreq);
-    console.log(`Washing Freq: ${wfreqArray}`)
+    var wfreqArray = data.metadata.filter(sampleObj => sampleObj.id == sample).map(d => d.wfreq);
+    
     
     // Create a variable that holds the first sample in the array.
     var result = resultArray[0]
 
     // 2. Create a variable that holds the first sample in the metadata array.
     var wfreq = wfreqArray[0]
-    console.log(wfreq)
+    
 
     // Create variables that hold the otu_ids, otu_labels, and sample_values.
     var otu_ids = result.otu_ids;
@@ -85,7 +85,7 @@ function buildCharts(sample) {
 
     // 3. Create a variable that holds the washing frequency.
     var wfreqFinal = parseFloat(wfreq);
-    console.log(wfreqFinal)
+    
 
     // Create the yticks for the bar chart.
     // Hint: Get the the top 10 otu_ids and map them in descending order 
